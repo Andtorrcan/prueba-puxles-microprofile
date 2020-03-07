e2a2761cb25cc078b1e4a78267933c2c999e1de8
Andrés Torres <Andtorrcan94@gmail.com>
2020-03-07 00:43:22 -0500

    Added swagger definition API

--- backend/src/main/java/com/prueba/pruebapuxlesmicroprofile/rest/RestApplication.java
+++ backend/src/main/java/com/prueba/pruebapuxlesmicroprofile/rest/RestApplication.java
@@ -3,6 +3,24 @@ package com.prueba.pruebapuxlesmicroprofile.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.SwaggerDefinition;
+
 @ApplicationPath("/")
+@SwaggerDefinition (
+info = @Info (
+        title = "Prueba técnica Puxless - Microprofile - MongoDB",
+        description = "3 tablas (persona, taller, habilidades), donde una persona puede hacer muchos talleres y un taller tiene muchas habilidades.",
+        version = "1.0.0",
+        contact = @Contact (
+            name = "Andres Torres",
+            email = "andtorrcan94@gmail.com"
+        )
+    ),
+    host = "localhost",
+    basePath = "/",
+    schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS}
+)
 public class RestApplication extends Application {
 }
