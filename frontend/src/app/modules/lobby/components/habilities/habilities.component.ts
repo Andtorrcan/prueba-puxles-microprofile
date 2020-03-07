3cc1bbf0d43704a3b60ac375ec35a8f2faab4b71
Andrés Torres <Andtorrcan94@gmail.com>
2020-03-07 14:44:03 -0500

    added component person, course and hability

---
+++ frontend/src/app/modules/lobby/components/habilities/habilities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habilities',
+  templateUrl: './habilities.component.html',
+  styleUrls: ['./habilities.component.scss']
+})
+export class HabilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
