ca25829b36f3fc7b283798d79ba9ba432210c2e6
Andrés Torres <Andtorrcan94@gmail.com>
2020-03-06 20:55:20 -0500

    added entity manager

---
+++ src/main/java/com/prueba/pruebapuxlesmicroprofile/rest/EntityManagerProducer.java
@@ -0,0 +1,20 @@
+package com.prueba.pruebapuxlesmicroprofile.rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+    @Produces
+    public EntityManager createEntityManager() {
+        return Persistence.createEntityManagerFactory("restapi-unit").createEntityManager();
+    }
+
+    public void close(EntityManager entityManager) {
+        entityManager.close();
+    }
+
+}
